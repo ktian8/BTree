82a4f235f3514a918d61c50a12bab8c91f45b4c3
techming <hmeng29@wisc.edu>
2022-03-07 00:51:48 -0600

    feat: constructor phase1

--- src/btree.cpp
+++ src/btree.cpp
@@ -32,7 +32,54 @@ BTreeIndex::BTreeIndex(const std::string & relationName,
 		const int attrByteOffset,
 		const Datatype attrType)
 {
+	std::ostringstream idxStr;
+	idxStr << relationName << '.' << attrByteOffset;
+	std::string indexName = idxStr.str();
+	outIndexName = indexName;
 
+	this->bufMgr = bufMgr;
+	this->attributeType = attrType;
+	this->attrByteOffset = attrByteOffset;
+	this->leafOccupancy = badgerdb::INTARRAYLEAFSIZE;
+	this->nodeOccupancy = badgerdb::INTARRAYNONLEAFSIZE;
+	
+	// Scanning related memebers
+	this->scanExecuting = false;
+	this->nextEntry = 0; // Need to confirm
+	this->lowValInt = 0;
+	this->lowValDouble = 0.0;
+	this->lowValString = "";
+	this->highValInt = 0;
+	this->highValDouble = 0.0;
+	this->highValString = "";
+	this->lowOp = badgerdb::Operator::LTE;
+	this->highOp = badgerdb::Operator::GTE;
+	
+	
+	try
+	{
+		File file = (File) BlobFile::open(outIndexName);
+
+		this->file = file;
+		this->headerPageNum = file.getFirstPageNo();
+
+		badgerdb::IndexMetaInfo *meta;
+		badgerdb::Page *page;
+		this->bufMgr->readPage(file, this->headerPageNum, page);
+		*meta = (badgerdb::IndexMetaInfo) page;
+		// Read root page number from the head (first page)
+		this->rootPageNum = meta->rootPageNo;
+		// Unpin the page after reading
+		this->bufMgr->unPinPage(file, this->headerPageNum, false);
+
+		this->currentPageNum = this->rootPageNum;
+		this->bufMgr->readPage(file, this->rootPageNum, this->currentPageData);
+	}
+	catch(const badgerdb::FileNotFoundException & e)
+	{
+		// build the index
+	}
+	
 }
 
 
